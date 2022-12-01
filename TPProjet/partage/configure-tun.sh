c7b9d0b8f3075c139eb5285c47069abd43470581
jinhua <tlsuibetheo@gmail.com>
2022-12-01 14:50:13 +0100

    2

--- TPProjet/partage/configure-tun.sh
+++ TPProjet/partage/configure-tun.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-ip -6 addr add $2/64 dev $1
-ip -6 link set $1 up
-sysctl -w net.ipv6.conf.all.forwarding=1
-# ip link set tun0 up
-# ip addr add fc00:1234:ffff::1/64 dev tun0
+ip link set tun0 up
+p addr add fc00:1234:ffff::1/64 dev tun0
